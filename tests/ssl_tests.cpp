de0fe595960c2241d0b1e6af6e98f370027f6a46
novemus <nineletters@mail.ru>
2022-04-26 00:11:36 +0300

    implemented test ssl server with boost

--- tests/ssl_tests.cpp
+++ tests/ssl_tests.cpp
@@ -1,39 +1,115 @@
 #include <future>
-#include <mutex>
+#include <cstdlib>
 #include <iostream>
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <openssl/ssl.h>
-#include <openssl/err.h>
+#include <boost/bind.hpp>
+#include <boost/asio.hpp>
+#include <boost/asio/ssl.hpp>
 #include <boost/test/unit_test.hpp>
 #include "../network.h"
 
-std::string get_last_error(const std::string& comment = "")
-{
-    auto ssl = ERR_get_error();
-    if (ssl != 0)
-        return comment + ": " + ERR_error_string(ssl, NULL);
-    
-    auto sys = errno;
-    if (sys != 0)
-        return comment + ": " + strerror(errno);
 
-    return comment;
-}
-
-class ssl_echo_server
+class ssl_echo_session
 {
-    SSL_CTX* m_context = 0;
-    int m_socket = -1;
-    std::future<void> m_work;
+    enum
+    {
+        max_length = 1024
+    };
+
+    char m_data[max_length];
+    boost::asio::ssl::stream<boost::asio::ip::tcp::socket> m_socket;
 
 public:
 
-    ssl_echo_server()
+    ssl_echo_session(boost::asio::io_service &io, boost::asio::ssl::context &ssl)
+        : m_socket(io, ssl)
     {
+    }
+
+    boost::asio::ssl::stream<boost::asio::ip::tcp::socket>::lowest_layer_type &socket()
+    {
+        return m_socket.lowest_layer();
+    }
+
+    void start()
+    {
+        m_socket.async_handshake(
+            boost::asio::ssl::stream_base::server,
+            boost::bind(&ssl_echo_session::handle_handshake, this, boost::asio::placeholders::error)
+            );
+    }
+
+    void handle_handshake(const boost::system::error_code &error)
+    {
+        if (!error)
+        {
+            m_socket.async_read_some(
+                boost::asio::buffer(m_data, max_length),
+                boost::bind(&ssl_echo_session::handle_read, this, boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred)
+                );
+        }
+        else
+        {
+            delete this;
+        }
+    }
+
+    void handle_read(const boost::system::error_code &error,
+                     size_t bytes_transferred)
+    {
+        if (!error)
+        {
+            boost::asio::async_write(
+                m_socket,
+                boost::asio::buffer(m_data, bytes_transferred),
+                boost::bind(&ssl_echo_session::handle_write, this, boost::asio::placeholders::error)
+                );
+        }
+        else
+        {
+            delete this;
+        }
+    }
+
+    void handle_write(const boost::system::error_code &error)
+    {
+        if (!error)
+        {
+            m_socket.async_read_some(
+                boost::asio::buffer(m_data, max_length),
+                boost::bind(&ssl_echo_session::handle_read, this, boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred)
+                );
+        }
+        else
+        {
+            delete this;
+        }
+    }
+};
+
+class ssl_echo_server
+{
+    std::future<void> m_work;
+    boost::asio::io_service m_io;
+    boost::asio::ip::tcp::acceptor m_acceptor;
+    boost::asio::ssl::context m_ssl;
+
+public:
+
+    ssl_echo_server(unsigned short port, const std::string& crt, const std::string& key, const std::string& ca = "")
+        : m_acceptor(m_io, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port)),
+          m_ssl(boost::asio::ssl::context::sslv23)
+    {
+        m_ssl.set_options(boost::asio::ssl::context::default_workarounds | boost::asio::ssl::context::no_sslv2);
+        m_ssl.use_certificate_file(crt, boost::asio::ssl::context::pem);
+        m_ssl.use_private_key_file(key, boost::asio::ssl::context::pem);
+        if (!ca.empty())
+        {
+            m_ssl.set_verify_mode(SSL_VERIFY_PEER);
+            m_ssl.load_verify_file(ca);
+        }
     }
 
     ~ssl_echo_server()
@@ -41,158 +117,67 @@ public:
         stop();
     }
 
-    void start(uint16_t port, const std::string& cert, const std::string& key, const std::string& ca = "")
+    void start()
     {
-        m_context = SSL_CTX_new(SSLv23_server_method());
-        if (!m_context)
-            throw std::runtime_error(get_last_error("SSL_CTX_new failed"));
-        
-        if (!SSL_CTX_use_certificate_file(m_context, cert.c_str(), SSL_FILETYPE_PEM))
-            throw std::runtime_error(get_last_error("SSL_CTX_use_certificate_file failed"));
-
-        if (!SSL_CTX_use_PrivateKey_file(m_context, key.c_str(), SSL_FILETYPE_PEM))
-            throw std::runtime_error(get_last_error("SSL_CTX_use_PrivateKey_file failed"));
-
-        if (!SSL_CTX_check_private_key(m_context))
-            throw std::runtime_error(get_last_error("SSL_CTX_check_private_key failed"));
-
-        if (!ca.empty())
-        {
-            SSL_CTX_set_verify(m_context, SSL_VERIFY_PEER | SSL_VERIFY_FAIL_IF_NO_PEER_CERT, 0);
-
-            if (!SSL_CTX_load_verify_locations(m_context, ca.c_str(), NULL))
-                throw std::runtime_error(get_last_error("SSL_CTX_load_verify_locations failed"));
-        }
-
-        sockaddr_in addr;
-        addr.sin_family = AF_INET;
-        addr.sin_port = htons(port);
-        addr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-        m_socket = socket(AF_INET, SOCK_STREAM, 0);
-        if (m_socket < 0)
-            throw std::runtime_error(strerror(errno));
-
-        int enable = 1;
-        if (setsockopt(m_socket, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0)
-            throw std::runtime_error(strerror(errno));
-
-        if (bind(m_socket, (sockaddr*)&addr, sizeof(addr)) < 0)
-            throw std::runtime_error(strerror(errno));
-
-        if (listen(m_socket, 1) < 0)
-            throw std::runtime_error(strerror(errno));
+        start_accept();
 
         m_work = std::async(std::launch::async, [this]()
         {
-            try
-            { 
-                do
-                {
-                    sockaddr_in addr;
-                    unsigned int len = sizeof(addr);
-                    int client = accept(m_socket, (sockaddr*)&addr, &len);
-                    if (client < 0)
-                    {
-                        int err = errno;
-                        if(err == EINVAL || err == EBADF)
-                            return;
-
-                        std::cerr << "unable to accept: " << strerror(err) << std::endl;
-                        return;
-                    }
-
-                    SSL* ssl = SSL_new(m_context);
-                    SSL_set_fd(ssl, client);
-
-                    int ret = SSL_accept(ssl);
-                    if (ret <= 0)
-                    {
-                        std::cerr << "SSL_accept failed: " << ERR_error_string(SSL_get_error(ssl, ret), NULL) << std::endl;
-                        return;
-                    }
-
-                    char buffer[1024];
-                    do
-                    {
-                        int size = SSL_read(ssl, buffer, 1024);
-                        if (size <= 0)
-                        {
-                            int err = SSL_get_error(ssl, size);
-                            if (err == SSL_ERROR_ZERO_RETURN)
-                            {
-                                break;
-                            }
-                            else if (err == SSL_ERROR_SYSCALL || err == SSL_ERROR_SSL)
-                            {
-                                std::cerr << "SSL_read failed: " << ERR_error_string(err, NULL) << std::endl;
-                                return;
-                            }
-                        }
-                        else
-                        {
-                            // std::cout << buffer << std::endl;
-                            size = SSL_write(ssl, buffer, size);
-                            if (size < 0)
-                            {
-                                std::cerr << "SSL_write failed: " << ERR_error_string(SSL_get_error(ssl, size), NULL) << std::endl;
-                                return;
-                            }
-                        }
-                    }
-                    while (true);
-
-                    SSL_shutdown(ssl);
-                    SSL_free(ssl);
-                    close(client);
-                }
-                while (true);
-            }
-            catch(const std::exception& e)
-            {
-                std::cerr << e.what() << std::endl;
-            }
+            m_io.run();
         });
     }
 
     void stop()
     {
-        if (m_socket >= 0)
+        if (!m_io.stopped())
+            m_io.stop();
+
+        if (m_work.valid())
+            m_work.wait();
+    }
+
+    void start_accept()
+    {
+        ssl_echo_session* session = new ssl_echo_session(m_io, m_ssl);
+        m_acceptor.async_accept(
+            session->socket(),
+            boost::bind(&ssl_echo_server::handle_accept, this, session, boost::asio::placeholders::error)
+            );
+    }
+
+    void handle_accept(ssl_echo_session *session, const boost::system::error_code &error)
+    {
+        if (!error)
         {
-            shutdown(m_socket, SHUT_RDWR);
-            close(m_socket);
-            m_socket = -1;
-
-            SSL_CTX_free(m_context);
-            m_context = 0;
-
-            if (m_work.valid())
-                m_work.wait();
+            session->start();
         }
+        else
+        {
+            delete session;
+        }
+
+        start_accept();
     }
 };
 
-
 BOOST_AUTO_TEST_CASE(no_check_certs)
 {
-    ssl_echo_server server;
-    BOOST_REQUIRE_NO_THROW(server.start(4433, "./certs/server.crt", "./certs/server.key"));
+    ssl_echo_server server(4433, "./certs/server.crt", "./certs/server.key");
+    BOOST_REQUIRE_NO_THROW(server.start());
 
-    std::shared_ptr<plexus::network::ssl> client(
-        plexus::network::create_ssl_client("127.0.0.1:4433")
-        );
+    std::shared_ptr<plexus::network::ssl> client(plexus::network::create_ssl_client("127.0.0.1:4433"));
     BOOST_REQUIRE_NO_THROW(client->connect());
 
     char buffer[1024];
 
     std::strcpy(buffer, "hello");
-    BOOST_REQUIRE_NO_THROW(BOOST_CHECK_EQUAL(client->write((uint8_t*)buffer, strlen(buffer) + 1), strlen(buffer) + 1));
-    BOOST_REQUIRE_NO_THROW(BOOST_CHECK_EQUAL(client->read((uint8_t*)buffer, sizeof(buffer)), strlen(buffer) + 1));
+    BOOST_REQUIRE_NO_THROW(BOOST_CHECK_EQUAL(client->write((uint8_t *)buffer, strlen(buffer) + 1), strlen(buffer) + 1));
+    BOOST_REQUIRE_NO_THROW(BOOST_CHECK_EQUAL(client->read((uint8_t *)buffer, sizeof(buffer)), strlen(buffer) + 1));
     BOOST_CHECK_EQUAL(std::strncmp(buffer, "hello", 1024), 0);
 
     std::strcpy(buffer, "bye bye");
-    BOOST_REQUIRE_NO_THROW(BOOST_CHECK_EQUAL(client->write((uint8_t*)buffer, strlen(buffer) + 1), strlen(buffer) + 1));
-    BOOST_REQUIRE_NO_THROW(BOOST_CHECK_EQUAL(client->read((uint8_t*)buffer, sizeof(buffer)), strlen(buffer) + 1));
+    BOOST_REQUIRE_NO_THROW(BOOST_CHECK_EQUAL(client->write((uint8_t *)buffer, strlen(buffer) + 1), strlen(buffer) + 1));
+    BOOST_REQUIRE_NO_THROW(BOOST_CHECK_EQUAL(client->read((uint8_t *)buffer, sizeof(buffer)), strlen(buffer) + 1));
     BOOST_CHECK_EQUAL(std::strncmp(buffer, "bye bye", 1024), 0);
 
     BOOST_REQUIRE_NO_THROW(client->shutdown());
@@ -201,9 +186,9 @@ BOOST_AUTO_TEST_CASE(no_check_certs)
 
 BOOST_AUTO_TEST_CASE(check_certs)
 {
-    ssl_echo_server server;
-    BOOST_REQUIRE_NO_THROW(server.start(4433, "./certs/server.crt", "./certs/server.key", "./certs/ca.crt"));
-    
+    ssl_echo_server server(4433, "./certs/server.crt", "./certs/server.key", "./certs/ca.crt");
+    BOOST_REQUIRE_NO_THROW(server.start());
+
     std::shared_ptr<plexus::network::ssl> client(
         plexus::network::create_ssl_client("127.0.0.1:4433", "./certs/client.crt", "./certs/client.key", "./certs/ca.crt")
         );
@@ -227,7 +212,7 @@ BOOST_AUTO_TEST_CASE(check_certs)
 BOOST_AUTO_TEST_CASE(wrong_server)
 {
     std::shared_ptr<plexus::network::ssl> client(
-        plexus::network::create_ssl_client("127.0.0.1:4422")
+        plexus::network::create_ssl_client("127.0.0.1:4433")
         );
     BOOST_REQUIRE_THROW(client->connect(), std::exception);
     BOOST_REQUIRE_NO_THROW(client->shutdown());
@@ -235,11 +220,11 @@ BOOST_AUTO_TEST_CASE(wrong_server)
 
 BOOST_AUTO_TEST_CASE(timeout)
 {
-    ssl_echo_server server;
-    BOOST_REQUIRE_NO_THROW(server.start(4433, "./certs/server.crt", "./certs/server.key"));
+    ssl_echo_server server(4433, "./certs/server.crt", "./certs/server.key");
+    BOOST_REQUIRE_NO_THROW(server.start());
     
     std::shared_ptr<plexus::network::ssl> client(
-        plexus::network::create_ssl_client("127.0.0.1:4433")
+        plexus::network::create_ssl_client("127.0.0.1:4433", "", "", "", 2)
         );
     BOOST_REQUIRE_NO_THROW(client->connect());
 
@@ -252,7 +237,8 @@ BOOST_AUTO_TEST_CASE(timeout)
     BOOST_REQUIRE_NO_THROW(server.stop());
 
     std::strcpy(buffer, "bye bye");
-    BOOST_REQUIRE_THROW(client->write((uint8_t*)buffer, strlen(buffer) + 1), std::exception);
+    BOOST_REQUIRE_NO_THROW(BOOST_CHECK_EQUAL(client->write((uint8_t*)buffer, strlen(buffer) + 1), strlen(buffer) + 1));
+    BOOST_REQUIRE_THROW(client->read((uint8_t*)buffer, sizeof(buffer)), std::exception);
 
     BOOST_REQUIRE_NO_THROW(client->shutdown());
 }
