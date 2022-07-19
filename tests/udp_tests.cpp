83ece25c6739cbd3bb34e86f11a3328caecb4a52
novemus <nineletters@mail.ru>
2022-07-19 08:05:12 +0300

    fixed license reference

--- tests/udp_tests.cpp
+++ tests/udp_tests.cpp
@@ -4,7 +4,8 @@
  * Licensed under the Apache License, Version 2.0 (the "License").
  * You may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * https://www.openssl.org/source/license.html
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  */
 
 #include <future>
